4a0aca4e541665d22270576a8d73997407d45d3c
guilherme-op7 <netoguilherme989@gmail.com>
2025-08-23 19:58:53 -0300

    subindo backend

--- api/index.js
+++ api/index.js
@@ -3,5 +3,5 @@ import server from '../src/server.js'
 const PORT = process.env.PORT || 4000
 
 server.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`)
+  console.log(`Servidor rodando na porta ${PORT}!`)
 })
